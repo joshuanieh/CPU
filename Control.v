be06a368d419528a3b943001be6d594ea208d586
joshuanieh <b07502027@ntu.edu.tw>
2021-11-07 14:54:18 +0800

    control

--- Control.v
+++ Control.v
@@ -5,3 +5,14 @@ module Control(
     RegWrite_o
 );
 
+// Ports
+input   [6:0]       Op_i;
+output  [1:0]       ALUOp_o;
+output              ALUSrc_o;
+output              RegWrite_o;
+
+assign RegWrite_o = 1'b1;
+assign ALUSrc_o = ~ Op_i[5];
+assign ALUOp_o = Op_i[6:5];
+
+endmodule
